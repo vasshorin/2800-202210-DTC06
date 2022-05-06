a77e69abe28ce3b7b1866ae478ace4f8caf6dfc9
Andy Lau <andyhplau@gmail.com>
2022-05-05 22:16:09 -0700

    Create template.html with navbars and footer

---
+++ public/scripts/skeleton.js
@@ -0,0 +1,14 @@
+// load skeleton to pages
+function loadSkeleton(){
+    console.log($('#topNavPlaceHolder').load('../skeletons/topNav.html'));
+    console.log($('#footerPlaceHolder').load('../skeletons/footer.html'));
+    console.log($('#bottomNavPlaceHolder').load('../skeletons/bottomNav.html'));
+}
+
+// call the functions
+function setup(){
+    loadSkeleton();
+}
+
+// call the setup function when page is ready
+$(document).ready(setup);
