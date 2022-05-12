9f2fed4f86a30d6a6550d0d27b89cb7eb9617239
Andy Lau <andyhplau@gmail.com>
2022-05-11 21:01:35 -0700

    Create CRUD testing template

--- server.js
+++ server.js
@@ -3,14 +3,11 @@ const app = express()
 const https = require('https')
 const bodyparser = require('body-parser')
 const mongoose = require('mongoose')
-const unicornSchema = new mongoose.Schema({
-    name: String,
-    weight: Number,
-    dob: Date,
-    loves: [String],
-    gender: String
+const testSchema = new mongoose.Schema({
+    user: String,
+    text: String
 });
-const unicornModel = mongoose.model("unicorns", unicornSchema);
+const testModel = mongoose.model("tests", testSchema);
 
 app.set('view engine', 'ejs')
 
@@ -18,11 +15,10 @@ app.use(bodyparser.urlencoded({
     extended: true
 }))
 
-mongoose.connect("mongodb+srv://andy:andy1993@ucan.gvfrz.mongodb.net/unicorns?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://andy:andy1993@ucan.gvfrz.mongodb.net/ucan?retryWrites=true&w=majority", {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
-
 
 app.listen(process.env.PORT || 5000, (err) => {
     if (err)
@@ -31,17 +27,65 @@ app.listen(process.env.PORT || 5000, (err) => {
 
 app.use(express.static('./public'))
 
-app.get('/', function (req, res) {
-    unicornModel.find({
-        weight: {
-            $gt: 500
+// CRUD
+
+// Create
+app.put('/test/create', function (req, res) {
+    console.log(req.body)
+    testModel.create({
+        user: req.body.user,
+        text: req.body.text
+    }, function (err, testData) {
+        if (err) {
+            console.log('Error' + err)
+        } else {
+            console.log('Data' + testData)
         }
-    }, function (err, unicorns) {
+        res.send('Data inserted!')
+    })
+})
+
+// Read
+app.get('/test/read', function (req, res) {
+    testModel.find({}, function (err, testData) {
         if (err) {
             console.log("Error" + err)
         } else {
-            console.log("Data" + unicorns)
+            console.log("Data" + testData)
         }
-        res.send(unicorns)
+        res.send(testData)
     })
 })
+
+// Update
+app.put('/test/update', function (req, res) {
+    console.log(req.body)
+    testModel.updateOne({
+        _id: req.body.id
+    }, {
+        $set: {
+            text: req.body.text
+        }
+    }, function (err, testData) {
+        if (err) {
+            console.log('Error' + err)
+        } else {
+            console.log('Data' + testData)
+        }
+        res.send('Data updated!')
+    })
+})
+
+// Delete
+app.get('/test/delete', function (req, res) {
+    testModel.remove({
+        _id: req.body.id,
+    }, function (err, testData) {
+        if (err) {
+            console.log("Error " + err);
+        } else {
+            console.log("Data " + testData);
+        }
+        res.send("Data deleted!");
+    });
+});
