3396a826ddfdb698c23327b7f20672fb5b5a1ff4
U-LFELIX\LieuFelix <flieu@my.bcit.ca>
2022-05-20 14:32:58 -0700

    houseListing.js changes

--- public/scripts/houseListing.js
+++ public/scripts/houseListing.js
@@ -7,30 +7,25 @@ var formatted = now.getHours() + ":" + now.getMinutes() + ":" + now.getSeconds()
 //     return userId
 // }
 
+userId = null
 
 
 // Current user ID
 function getUserId(){
-    var userId = null
     $.ajax({
         url: "http://localhost:5002/userId",
         type: "GET",
-        success: (userObj)=>{
-            userID = userObj._id
-        }
-        })
-        return userID
-        console.log(userID)
+        success: loadEventsToMainDiv
+    })
+        // success: (userObj)=>{
+        //     userId = userObj
+        // }
+        // })
+        // return userId
 }
-
-
-
-
 
 // submit form            
 // userId: req.session.userobj._id,
-
-
 function submitForm() {
     var title = $("#postTitle").val();
     var description = $("#postBody").val();
@@ -54,51 +49,54 @@ function submitForm() {
     })
 }
 
-function loadEventsToMainDiv(userId) {
-    console.log("the userId: " + userId._id)
-    // var post = 1;
+function loadEventsToMainDiv(userObj) {
+    userID = userObj._id;
+    console.log("the userId: " + userObj._id);
+    var post = 1;
     // $.ajax({
     //     url: "http://localhost:5002/userId",
     //     type: "GET",
     //     success: storeUserObj
     //     userId = userObj._id
     //     })
-    // $.ajax({
-    //     url: "http://localhost:5002/test/read",
-    //     type: "get",
-    //     success: (r)=>{
-    //         console.log(r)
-    //         for( i = 0 ; i < r.length; i++ ){
-    //             if (userId == r[i].userId){
-    //             $("main").append(`
-    //             <div id="posts">
-    //     <div class="post">
-    //         <div class="post-header">
-    //             <h1>
-    //                 Post: <span id=title>${r[i].title}</span>
-    //             </h1>
-    //         </div>
-    //         <div class="post-body">
-    //         <p> Price $<span id=price>${r[i].price}</span></p>
-    //             <p>
-    //                 ${r[i].description}
-    //             </p>
-    //             <hr>
 
-    //             <button class="deleteButtons" id="${r[i]["_id"]}"> Delete</button> 
-    //             <button class="saveButtons" id="${r[i]["_id"]}"> Save</button>
-    //             <button class="publishButtons" id="${r[i]["_id"]}"> Publish</button>
-    //         </div>
-    //     </div>     
-    //                 `)
-    //                 post++;  
-    //                 // console.log("Price: " + r[i].price)   
-    //                 // console.log("Id: " + r[i]["_id"])    
-    //         }
-    //     }
+    $.ajax({
+        url: "http://localhost:5002/test/read",
+        type: "get",
+        success: (r)=>{
+            // console.log(r)
+            // if (userId == r[i].userId)
+            for( i = 0 ; i < r.length; i++ ){
+                {
+                $("main").append(`
+                <div id="posts">
+                <div class="post">
+                <div class="post-header">
+                <h1>
+                    Post: <span id=title>${r[i].title}</span>
+                </h1>
+            </div>
+            <div class="post-body">
+            <p> Price $<span id=price>${r[i].price}</span></p>
+                <p>
+                    ${r[i].description}
+                </p>
+                <hr>
+
+                <button class="deleteButtons" id="${r[i]["_id"]}"> Delete</button> 
+                <button class="saveButtons" id="${r[i]["_id"]}"> Save</button>
+                <button class="publishButtons" id="${r[i]["_id"]}"> Publish</button>
+            </div>
+        </div>     
+                    `)
+                    post++;  
+                    // console.log("Price: " + r[i].price)   
+                    // console.log("Id: " + r[i]["_id"])    
+            }
+        }
            
-    //     }
-    // })
+        }
+    })
 
 }
 
@@ -118,12 +116,11 @@ function loadEventsToMainDiv(userId) {
 // }
 
 function setup() {
-    console.log("setup")
     getUserId()
-    loadEventsToMainDiv()
+    // loadEventsToMainDiv()
 
     $("body").on("click", "#submit", submitForm)
-    $("body").on("click", ".deleteButtons", deleteEvent)
+    // $("body").on("click", ".deleteButtons", deleteEvent)
 }
 
 
